45c65775d80552df4c25a86cca1693fe6e5b9526
Jisoo Kim <jisoo.kim1710@gmail.com>
2024-05-01 17:12:51 -0700

    take out user ID

--- database.sql
+++ database.sql
@@ -4,9 +4,8 @@ CREATE DATABASE cluster;
 USE cluster;
 
 CREATE TABLE user_accounts (
-    user_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     -- username: can ONLY contain English letters (case-insensitive), digits, _, -
-    username VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) PRIMARY KEY,
     -- email: well foramtted (make sure @ exist and . exist at certain areas)
     email VARCHAR(255) UNIQUE NOT NULL,
     -- have my own limitations
@@ -16,6 +15,6 @@ CREATE TABLE user_accounts (
 CREATE TABLE input_models (
     model_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     model_name VARCHAR(255) NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user_accounts(user_id)
+    username VARCHAR(255) NOT NULL,
+    FOREIGN KEY (username) REFERENCES user_accounts(username)
 );
